e6765e5e31c29055abb25eeb7c13e6f20904f378
Giulia Franca <giuliaa.francaa@gmail.com>
2023-03-30 18:16:40 -0300

    Agora codigo com a função de deletar atleta.

--- app/src/main/java/br/com/senaicimatec/agualife/HistoricoActivity.java
+++ app/src/main/java/br/com/senaicimatec/agualife/HistoricoActivity.java
@@ -6,23 +6,32 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class HistoricoActivity extends AppCompatActivity {
     private SQLiteDatabase bancoDados;
     public ListView list_dados;
-
+    public ArrayList<Integer> arrayIds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historico);
 
         list_dados = (ListView) findViewById(R.id.list_dados);
-
+        list_dados.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                DeletarItem(i);
+                return true;
+            }
+        });
         criarBanco();
         listarDados();
     }
@@ -44,7 +53,7 @@ public class HistoricoActivity extends AppCompatActivity {
 
     public void listarDados(){
         try {
-            // ArrayIds = new ArrayList<>();
+            arrayIds = new ArrayList<>();
             bancoDados = openOrCreateDatabase("crud", MODE_PRIVATE, null);
             Cursor cursor = bancoDados.rawQuery("SELECT id, nome, peso, idade from tb_atleta", null);
             ArrayList<String> atleta = new ArrayList<String>();
@@ -57,12 +66,11 @@ public class HistoricoActivity extends AppCompatActivity {
             list_dados.setAdapter(adapter);
             cursor.moveToFirst();
             while (cursor != null){
-                String atletas = cursor.getString(0) + " : " +
-                        cursor.getString(1) + " - " +
+                String atletas = cursor.getString(1) + " - " +
                         cursor.getString(2) + " Kg e " +
                         cursor.getString(3) + " anos.";
                 atleta.add(atletas);
-                // ArrayIds.add(cursor.getInt(0));
+                arrayIds.add(cursor.getInt(0));
                 cursor.moveToNext();
             }
         } catch (Exception e){
@@ -70,4 +78,18 @@ public class HistoricoActivity extends AppCompatActivity {
         }
     }
 
+    public void DeletarItem(Integer i){
+        try{
+            bancoDados = openOrCreateDatabase("crud", MODE_PRIVATE, null);
+            String sql = "DELETE FROM tb_atleta WHERE id=?";
+            SQLiteStatement stmt = bancoDados.compileStatement(sql);
+            stmt.bindLong(1, arrayIds.get(i));
+            stmt.executeUpdateDelete();
+            listarDados();
+            bancoDados.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        Toast.makeText(this, "Atleta Deletado", Toast.LENGTH_SHORT).show();
+    }
 }
